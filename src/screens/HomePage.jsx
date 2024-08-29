b21a1c8e28ed6508d1134ea8763fcab9374750d3
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-29 17:43:26 +0530

    Carousel ui

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import AboveImg from "../components/AboveImg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 10,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 5,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 3,
+  },
+};
 
 const HomePage = () => {
   return (
@@ -23,12 +44,81 @@ const HomePage = () => {
           <Navbar />
         </div>
 
-        <div className="relative z-10 mt-[40vh] lg:mt-[90vh]">
-          <h1 className="text-3xl">Heloo</h1>
-          <h1 className="text-3xl">Heloo</h1>
-          <h1 className="text-3xl">Heloo</h1>
-          <h1 className="text-3xl">Heloo</h1>
-          <h1 className="text-3xl">Heloo</h1>
+        <div className="relative z-10 mt-[40vh] lg:mt-[93vh] mb-5">
+          <Carousel
+            showDots={true}
+            arrows={false}
+            responsive={responsive}
+            draggable={false}
+            autoPlay={true}
+          >
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5 mb-8"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+            <img
+              src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-002.png"
+              alt="img"
+              className="border-2 border-gray-300 mx-5"
+            />
+          </Carousel>
+        </div>
+        <div>
+          <h1>efnrjbdvjrwe</h1>
+          <h1>efnrjbdvjrwe</h1>
+          <h1>efnrjbdvjrwe</h1>
+          <h1>efnrjbdvjrwe</h1>
         </div>
       </div>
     </>
