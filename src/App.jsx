f7480256e39c3c60b6811ddee79255034d7c5aa1
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-29 14:50:10 +0530

    react-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./screens/HomePage";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ src/screens/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='text-4xl'>HomePage</div>
+  )
+}
+
+export default HomePage
