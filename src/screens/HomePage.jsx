458784e099e9444793e9a4ec01d151e2d3290b3f
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-29 16:00:39 +0530

    working with mt

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -4,7 +4,23 @@ import Navbar from "../components/Navbar";
 const HomePage = () => {
   return (
     <>
-      <Navbar />
+      <div className="relative h-[60vh] lg:h-[90vh]">
+        <img
+          src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/hero-01.jpg"
+          alt="bg-image"
+          className="absolute w-full h-full object-cover"
+          style={{ zIndex: "-1" }}
+        />
+        <div className="relative z-10">
+          <Navbar />
+        </div>
+        <div className="mt-[50vh] lg:mt-[80vh]">
+          <h1 className="text-3xl">Vishal Kumawat</h1>
+          <h1 className="text-3xl">Vishal Kumawat</h1>
+          <h1 className="text-3xl">Vishal Kumawat</h1>
+          <h1 className="text-3xl">Vishal Kumawat</h1>
+        </div>
+      </div>
     </>
   );
 };
