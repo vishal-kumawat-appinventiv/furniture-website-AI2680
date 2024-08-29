8e3b02fe9a29faf3da7456f9e40374c4c4b9959c
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-30 05:07:12 +0530

    footer v2 completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
+import { Facebook, Instagram, Twitter, Youtube } from "lucide-react";
 import React from "react";
 
 const Footer = () => {
   return (
     <>
-      <div className="flex items-center justify-center bg-[#f1f2f2] ">
+      <div className="flex items-center justify-center bg-[#f1f2f2] border-b-2">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl mt-12 mb-32 gap-5 p-2">
           <img
             src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-regular.png"
@@ -35,6 +36,17 @@ const Footer = () => {
             <button className="p-3 mt-3 w-32 text-white bg-black">
               Subscribe
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#f1f2f2]">
+        <div className="max-w-7xl mx-auto flex flex-col gap-5 sm:flex-row items-center justify-between p-10">
+          <p>Copyright © 2024 Furniture Shop | Powered by Furniture Shop</p>
+          <div className="flex gap-2">
+            <Facebook />
+            <Youtube />
+            <Instagram />
+            <Twitter />
           </div>
         </div>
       </div>
