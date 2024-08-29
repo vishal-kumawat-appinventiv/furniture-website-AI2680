4287ea325d416a65cf95056e7a5dd7d7b7608474
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-30 02:29:43 +0530

    working now

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -86,46 +86,58 @@ const HomePage = () => {
   return (
     <>
       <div className="relative h-screen">
-        <div className="absolute top-0 left-0 h-[50vh] lg:h-[100vh] w-full">
+        <div className="fixed top-0 left-0 h-[50vh] lg:h-[100vh] w-full">
           <img
             src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/hero-01.jpg"
             alt="bg-image"
             className="w-full h-full object-cover"
             style={{ zIndex: "-1" }}
           />
-
-          <div className="absolute top-[40%] left-[15%] z-10">
-            <AboveImg />
-          </div>
         </div>
 
         <div className="relative z-10">
           <Navbar />
         </div>
 
-        <div className="relative z-10 mt-[42vh] lg:mt-[93vh] mb-5">
-          <Carousel
-            showDots={true}
-            arrows={false}
-            responsive={responsive}
-            draggable={false}
-            autoPlay={true}
-            itemClass="mx-2"
-          >
-            {CarouselImages.map((i, idx) => {
-              return (
-                <img
-                  key={idx}
-                  src={i?.image}
-                  alt="img"
-                  className="border-2 border-gray-300 mb-8"
-                />
-              );
-            })}
-          </Carousel>
+        <div className="absolute top-[20%] left-[7%] lg:top-[40%] lg:left-[15%] z-10">
+          <AboveImg />
         </div>
-        <div>
-          <h1>efnrjbdvjrwe</h1>
+
+        <div className="relative z-10 bg-white">
+          <div className="mt-[42vh] lg:mt-[100vh] mb-5 pt-5">
+            <Carousel
+              showDots={true}
+              arrows={false}
+              responsive={responsive}
+              draggable={false}
+              autoPlay={true}
+              itemClass="mx-2"
+            >
+              {CarouselImages.map((i, idx) => {
+                return (
+                  <img
+                    key={idx}
+                    src={i?.image}
+                    alt="img"
+                    className="border-2 border-gray-300 mb-8"
+                  />
+                );
+              })}
+            </Carousel>
+          </div>
+
+          <div className="p-4">
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+            <h1>efnrjbdvjrwe</h1>
+          </div>
         </div>
       </div>
     </>
