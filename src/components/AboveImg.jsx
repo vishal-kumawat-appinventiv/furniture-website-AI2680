05c841b61a38d40cf3ab802d70566317d4ab2235
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-29 16:55:07 +0530

    ui

---
+++ src/components/AboveImg.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboveImg = () => {
+  return (
+    <div className="flex flex-col gap-3">
+      <p>Black Friday in july</p>
+      <h1 className="text-3xl lg:text-7xl font-bold text-black">Up to 50% off</h1>
+      <h4 className="text-xl text-black">Hundreds of styles available</h4>
+      <button className="p-4 w-32 bg-black text-white">SHOP NOW</button>
+    </div>
+  );
+};
+
+export default AboveImg;
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import AboveImg from "../components/AboveImg";
 
 const HomePage = () => {
   return (
     <>
       <div className="relative h-screen">
-        <img
-          src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/hero-01.jpg"
-          alt="bg-image"
-          className="absolute w-full h-[50vh] lg:h-[100vh] object-cover"
-          style={{ zIndex: "-1" }}
-        />
-        <div className="z-10">
+        <div className="absolute top-0 left-0 h-[50vh] lg:h-[100vh] w-full">
+          <img
+            src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/hero-01.jpg"
+            alt="bg-image"
+            className="w-full h-full object-cover"
+            style={{ zIndex: "-1" }}
+          />
+
+          <div className="absolute top-[40%] left-[15%] z-10">
+            <AboveImg />
+          </div>
+        </div>
+
+        <div className="relative z-10">
           <Navbar />
         </div>
-        <div className="mt-[40vh] lg:mt-[90vh]">
-          <h1 className="text-3xl">Vishal Kumawat</h1>
-          <h1 className="text-3xl">Vishal Kumawat</h1>
-          <h1 className="text-3xl">Vishal Kumawat</h1>
-          <h1 className="text-3xl">Vishal Kumawat</h1>
-        </div>
+
       </div>
     </>
   );
