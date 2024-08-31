699e5dd75c39f1d8127bbe41dff99424c83ebcc3
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-09-01 03:38:46 +0530

    ui

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -30,7 +30,11 @@ const ProductScreen = () => {
       <div className="max-w-7xl mx-auto p-4">
         <div className="mainGrid grid grid-cols-1 md:grid-cols-2 gap-12 mt-8">
           <div className="productImage">
-            <img src={images[0]} alt="main-image" />
+            <img src={images[0]} alt="main-image" className="mb-4" />
+            <div className="grid grid-cols-2 gap-4">
+              <img src={images[1]} alt="bottom-img" />
+              <img src={images[1]} alt="bottom-img" />
+            </div>
           </div>
           <div className="productDesc flex flex-col gap-2">
             <div className="flex items-center justify-between">
