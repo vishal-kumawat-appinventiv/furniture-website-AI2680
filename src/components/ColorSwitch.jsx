94010d74cf5643437c61491aa625aa0440e52266
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-09-01 03:23:05 +0530

    ui

--- src/components/ColorSwitch.jsx
+++ src/components/ColorSwitch.jsx
@@ -4,9 +4,9 @@ const ColorSwitch = () => {
   return (
     <>
       <div className="flex gap-2 items-center justify-center">
-        <p className="bg-black rounded-full p-2 w-2 border-white border-2"></p>
-        <p className="bg-[#8f6453] rounded-full p-2 w-2 border-white border-2"></p>
-        <p className="bg-[#dabca2] rounded-full p-2 w-2 border-white border-2"></p>
+        <p className="bg-black rounded-full p-2 w-2 border-white border-2 cursor-pointer"></p>
+        <p className="bg-[#8f6453] rounded-full p-2 w-2 border-white border-2 cursor-pointer"></p>
+        <p className="bg-[#dabca2] rounded-full p-2 w-2 border-white border-2 cursor-pointer"></p>
       </div>
     </>
   );
